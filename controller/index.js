ee9895243bf8da6920c8ba737e354124435dbe96
vienchu810 <vienchelsea325@gmail.com>
2024-11-14 11:08:06 +0700

    vmib

--- controller/index.js
+++ controller/index.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 // Tạo pool kết nối với PostgreSQL
 const pool = new Pool({
     user: 'postgres',
-    password: '123456',
-    host: '192.168.1.40',
+    password: 'a123456',
+    host: '209.126.5.177',
     port: '5432',
-    database: 'banhang',
+    database: 'postgres',
 });
 
 // Kiểm tra kết nối
